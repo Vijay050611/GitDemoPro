5d1e48bde508eae3623653dd7bff7c5297a65463
vijayanand.pilla <vijayanand.pilla@HYDPCM323632L.ad.infosys.com>
2022-07-12 18:01:41 +0530

    initial commit

---
+++ src/pack/Hung.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Hung {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean hungry=true;
+		if(hungry)
+			System.out.println("Go and eat");
+		else
+			System.out.println("continue doing work!");
+
+	}
+
+}
